892e74b7d45c0c964fd43eba9b421f8393d39a56
karthick <karthickisking@gmail.com>
2020-12-23 01:03:19 +0800

    Changed Sysouts To Logger

--- src/main/java/com/karthick/company/HelloSpringBoot/api/server/WelcomeSpringBoot.java
+++ src/main/java/com/karthick/company/HelloSpringBoot/api/server/WelcomeSpringBoot.java
@@ -2,14 +2,21 @@ package com.karthick.company.HelloSpringBoot.api.server;
 
 import java.time.LocalDate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class WelcomeSpringBoot {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(WelcomeSpringBoot.class);
 
 	@RequestMapping(value = "/api/newking")
 	public String welcomeSpring() {
+		LOG.info("Karthick, Welcome Back To Spring World");
 		return "Karthick, Welcome Back To Spring World";
 	}
 
